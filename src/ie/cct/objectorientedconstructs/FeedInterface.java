0e06bc7e7eb6922a9231607080b2de135ff47640
wfgv <willian.voelz@gmail.com>
2020-03-10 12:18:48 +0000

    Initial commit

---
+++ src/ie/cct/objectorientedconstructs/FeedInterface.java
@@ -0,0 +1,45 @@
+package ie.cct.objectorientedconstructs;
+
+import java.util.Collection;
+
+public interface FeedInterface {
+
+  /**
+   * This method returns a collection of the titles of all the
+   * items in the feed.
+   * 
+   * @return a list of titles of all the items in the feed.
+   */
+  public Collection<String> listTitles();
+  
+  /**
+   * This method returns the FeedItem with the given title, or 
+   * null if no item has the given title. You can assume that 
+   * no two items will have the same title 
+   * 
+   * @param  title the title of the item to find.
+   * @return the FeedItem with the given title or null if no 
+   *         item has the given title.
+   */
+  public FeedItem getItem(String title);
+  
+  /**
+   * This method returns the number of items in the feed.
+   * 
+   * @return the number of items in this feed.
+   */
+  public int numItems();
+  
+  /**
+   * This method returns a collection of all the items that 
+   * have the given keyword in either the title or the 
+   * content of the item. If no matching item is found, the
+   * method should return an empty collection.
+   * 
+   * @param keyword the keyword to search for.
+   * @return a collection of all the items that have the given
+   *         keyword in the title or the content.
+   */
+  public Collection<FeedItem> findItems(String keyword);
+  
+}
