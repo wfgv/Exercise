1155230d0ac9f0ff59d5764aa4551e9cdb002bcc
wfgv <willian.voelz@gmail.com>
2020-03-10 13:30:46 +0000

    Feed Class methods v2
    Update whole project

--- src/ie/cct/objectorientedconstructs/s2018447s2018245/Feed.java
+++ src/ie/cct/objectorientedconstructs/s2018447s2018245/Feed.java
@@ -1,22 +1,35 @@
 package ie.cct.objectorientedconstructs.s2018447s2018245;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 
 import ie.cct.objectorientedconstructs.FeedInterface;
 import ie.cct.objectorientedconstructs.FeedItem;
 
+//@author Willian Voelz 2018447
+
 public class Feed implements FeedInterface {
+	
+	
+	
+	private Collection<FeedInterface> feedItems;
+	
+	public Feed (String title, List<FeedInterface> feedItems) {
+		this.feedItems = feedItems;
+		
+	}
 
 	@Override
 	public Collection<String> listTitles() {
 		// TODO Auto-generated method stub
-		return null;
+		return this.feedItems;
 	}
 
 	@Override
 	public FeedItem getItem(String title) {
 		// TODO Auto-generated method stub
-		return null;
+		return this.feedItems.get(title);
 	}
 
 	@Override
