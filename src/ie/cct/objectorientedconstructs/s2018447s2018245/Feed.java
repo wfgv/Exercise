c425f952ae96237528b81a7e10a10b740a455920
martinsjr1976 <48283883+martinsjr1976@users.noreply.github.com>
2020-03-10 13:29:56 +0000

    Read file

--- src/ie/cct/objectorientedconstructs/s2018447s2018245/Feed.java
+++ src/ie/cct/objectorientedconstructs/s2018447s2018245/Feed.java
@@ -9,25 +9,21 @@ public class Feed implements FeedInterface {
 
 	@Override
 	public Collection<String> listTitles() {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public FeedItem getItem(String title) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public int numItems() {
-		// TODO Auto-generated method stub
 		return 0;
 	}
 
 	@Override
 	public Collection<FeedItem> findItems(String keyword) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
--- src/ie/cct/objectorientedconstructs/s2018447s2018245/FeedFactory.java
+++ src/ie/cct/objectorientedconstructs/s2018447s2018245/FeedFactory.java
@@ -1,17 +1,34 @@
 package ie.cct.objectorientedconstructs.s2018447s2018245;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 
 import ie.cct.objectorientedconstructs.FeedFactoryInterface;
 import ie.cct.objectorientedconstructs.FeedInterface;
 
+//@author Edwards Jr 2018245
+
 public class FeedFactory implements FeedFactoryInterface {
+
+	public FeedFactory() {
+
+		File file = new File("test.rss");
+		try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public FeedInterface createFeed(BufferedReader in) throws IOException {
-		// TODO Auto-generated method stub
 		return null;
+
 	}
 
 }
